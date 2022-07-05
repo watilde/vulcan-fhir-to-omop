b2850af0038cc600744ad3a46e65bb3632707848
Daijiro Wachi <daijiro.wachi@gmail.com>
2022-07-05 16:24:43 +0900

    add key placer

--- src/App.js
+++ src/App.js
@@ -13,6 +13,18 @@ import fhirpath from "fhirpath";
 import DEMO_DATA from "./data/example.json";
 import MAP_DATA from "./mapping/R4Bto5.4.json"
 
+function keyPlacer (key, value, obj) {
+  if (key.includes('.')) {
+    const keys = key.split('.');
+    const curKey = keys.slice(0, 1);
+    const newKey = keys.slice(1);
+    obj[curKey] = obj[curKey] || {};
+    keyPlacer(newKey, value, obj[curKey])
+  } else {
+    obj[key] = value;
+  }
+}
+
 function App() {
   const [data, setData] = useState(JSON.stringify(DEMO_DATA, null, 2));
   const [out, setOut] = useState();
@@ -21,10 +33,10 @@ function App() {
     for (const key in MAP_DATA) {
       if (Array.isArray(MAP_DATA[key])) {
         (MAP_DATA[key]).forEach((item) => {
-          out[item] = fhirpath.evaluate(JSON.parse(data), key);
+          keyPlacer(item, fhirpath.evaluate(JSON.parse(data), key), out);
         });
       } else {
-        out[MAP_DATA[key]] = fhirpath.evaluate(JSON.parse(data), key);
+        keyPlacer(MAP_DATA[key], fhirpath.evaluate(JSON.parse(data), key), out);
       }
     }
     setOut(JSON.stringify(out, null, 2));
